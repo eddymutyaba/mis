ed86efa08ec7feee8e4951d6ba80d6a0c684c1c5
eddymutyaba <eddymutyaba@gmail.com>
2021-08-11 23:03:55 +0300

    creating path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // clearconsole.log(os.platform());
 //console.log(car)
 const express = require('express');
+const path = require ("path")
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.urlencoded({extended:false}))
